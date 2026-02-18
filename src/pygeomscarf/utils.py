99639e4c05db93ec8b46ddfb756956cfb2b1add4
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2026-02-18 23:11:42 +0000

    fix copilit comments messing up stuff

--- src/pygeomscarf/utils.py
+++ src/pygeomscarf/utils.py
@@ -5,6 +5,7 @@ from collections.abc import Container
 from importlib import resources
 
 import pyg4ometry
+from dbetto import AttrsDict
 from pyg4ometry import geant4
 
 from . import core
@@ -44,6 +45,8 @@ def _place_pv(
 
     geant4.PhysicalVolume([0, 0, 0], [0, 0, z_pos, "mm"], log, name, mother, registry=reg)
 
+
+def merge_configs(base: AttrsDict, extra: AttrsDict | None) -> AttrsDict:
     """Merge two configuration dictionaries into a new one.
 
     The returned configuration contains all entries from ``base`` with values from
